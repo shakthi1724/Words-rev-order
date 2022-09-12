0d82410ed9f5a327b674ca99be62a1a7c7e44139
siva <sivashakthiarun.c@gmail.com>
2022-09-12 16:32:35 +0530

    w rev

---
+++ main.js
@@ -0,0 +1,48 @@
+// var n=prompt("Enter the number:");
+// function shakthi(num){
+//     if(num==0){
+//         rs="zero";
+//     }
+//     else if(num==1){
+//         rs="one";
+//     }
+//     else if(num==2){
+//         rs="two";
+//     }
+//     else if(num==3){
+//         rs="three";
+//     }
+//     else if(num==4){
+//         rs="four";
+//     }
+//     else if(num==5){
+//         rs="five";
+//     }
+//     else if(num==6){
+//         rs="six";
+//     }
+//     else if(num==7){
+//         rs="seven";
+//     }
+//     else if(num==8){
+//         rs="eight";
+//     }
+//     else if(num==9){
+//         rs="nine";
+//     }
+//     return rs;
+// }
+// var number=n;
+// var a;
+// var rem;
+// while(number!=0){
+//     rem=number%10;
+//     a=shakthi(rem);
+//     b=a;
+//     number=(number-rem)/10;
+//     console.log(b);
+// }
+
+// let a= document.getElementById("ajay");
+let a = document.getElementsByClassName("ajay")
+console.log(a);
